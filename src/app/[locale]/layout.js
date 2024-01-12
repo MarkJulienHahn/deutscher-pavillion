e723b86acbf75a9923f1565c19d41b4a843c96ca
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-12 14:33:37 +0100

    consentmanager

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,20 +1,23 @@
 import Nav from "../../../components/Nav/Nav";
 import Script from "next/script";
+import Head from "next/head";
 import "../globals.css";
 
 export default function LocaleLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
+      <Script
+        async
+        strategy="beforeInteractive"
+        src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
+        data-cmp-ab="1"
+        data-cmp-host="d.delivery.consentmanager.net"
+        data-cmp-cdn="cdn.consentmanager.net"
+        data-cmp-codesrc="1"
+      />
       <body>
         <Nav locale={locale} />
         {children}
-        <Script
-          src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
-          data-cmp-ab="1"
-          data-cmp-host="d.delivery.consentmanager.net"
-          data-cmp-cdn="cdn.consentmanager.net"
-          data-cmp-codesrc="1"
-        />
       </body>
     </html>
   );
