0fc9666536a27498fde8d1006ebc1ddfa70e23d9
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-15 13:24:34 +0100

    google tag manager

---
+++ components/cookieconsent.js
@@ -0,0 +1,18 @@
+"use client"
+
+import Script from 'next/script';
+
+const CookieConsentScript = () => {
+  return (
+    <Script
+      src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
+      data-cmp-ab="1"
+      data-cmp-host="d.delivery.consentmanager.net"
+      data-cmp-cdn="cdn.consentmanager.net"
+      data-cmp-codesrc="1"
+      strategy="afterInteractive"
+    />
+  );
+};
+
+export default CookieConsentScript;
--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -6,16 +6,20 @@ import "../globals.css";
 export default function LocaleLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
-      <Head>
-        {/* <script
-          type="text/javascript"
-          data-cmp-ab="1"
-          src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
-          data-cmp-host="d.delivery.consentmanager.net"
-          data-cmp-cdn="cdn.consentmanager.net"
-          data-cmp-codesrc="1"
-        ></script> */}
-      </Head>
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-MKL5XR2J');`,
+        }}
+      />
       {/* <Script
         async
         strategy="beforeInteractive"
