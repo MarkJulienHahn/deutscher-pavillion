a2d37db6c622292be9b99efe1f7617364c0d7861
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-15 23:33:52 +0100

    logos hardcoded

--- components/Thanks/Thanks.js
+++ components/Thanks/Thanks.js
@@ -5,6 +5,10 @@ import NavMenu from "../Nav/NavMenu";
 import { urlFor } from "../../hooks/useImageUrlBuilder";
 import Image from "next/image";
 
+import logo1 from "../../public/svg/AA_logo_black.svg";
+import logo2 from "../../public/svg/bw_logo_black.svg";
+import logo3 from "../../public/svg/ifa_logo_black.svg";
+
 export default function Thanks({ thanks, locale }) {
   const [delay, setDelay] = useState(true);
 
@@ -26,6 +30,23 @@ export default function Thanks({ thanks, locale }) {
             value={locale == "de" ? thanks.text.german : thanks.text.english}
           />
           <div className="dankLogos">
+            <Image
+              src={logo1}
+              alt="logo"
+              style={{ width: "250px", height: "100px" }}
+            />
+            <Image
+              src={logo2}
+              alt="logo"
+              style={{ width: "250px", height: "100px" }}
+            />
+            <Image
+              src={logo3}
+              alt="logo"
+              style={{ width: "250px", height: "100px" }}
+            />
+          </div>
+          {/* <div className="dankLogos">
             {thanks.logos.map((logo, i) => (
               <span
                 key={i}
@@ -45,7 +66,7 @@ export default function Thanks({ thanks, locale }) {
                 )}
               </span>
             ))}
-          </div>
+          </div> */}
           <div>
             <h2>{partnerHeadline}</h2>
             <PortableText value={partnerText} />
