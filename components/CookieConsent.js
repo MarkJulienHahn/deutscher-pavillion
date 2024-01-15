9bf01ec015f1ebe56c5e746adaf9755d135a3a36
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-15 13:56:12 +0100

    ...

--- components/CookieConsent.js
+++ components/CookieConsent.js
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
-import Script from 'next/script';
+import Script from "next/script";
 
 const CookieConsentScript = () => {
   return (
-    // <Script
-    //   src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
-    //   data-cmp-ab="1"
-    //   data-cmp-host="d.delivery.consentmanager.net"
-    //   data-cmp-cdn="cdn.consentmanager.net"
-    //   data-cmp-codesrc="1"
-    //   strategy="afterInteractive"
-    // />
+    <Script
+      src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
+      data-cmp-ab="1"
+      data-cmp-host="d.delivery.consentmanager.net"
+      data-cmp-cdn="cdn.consentmanager.net"
+      data-cmp-codesrc="1"
+      strategy="beforeInteractive"
+    />
   );
 };
 
