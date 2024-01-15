b447f2d54921513358a43c591dc2aff61bc5c97a
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-15 10:53:12 +0100

    noscript

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -7,14 +7,14 @@ export default function LocaleLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
       <Head>
-        <script
+        {/* <script
           type="text/javascript"
           data-cmp-ab="1"
           src="https://cdn.consentmanager.net/delivery/autoblocking/04ba2a2631d12.js"
           data-cmp-host="d.delivery.consentmanager.net"
           data-cmp-cdn="cdn.consentmanager.net"
           data-cmp-codesrc="1"
-        ></script>
+        ></script> */}
       </Head>
       {/* <Script
         async
