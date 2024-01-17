07fd5206278740b36c37d112c6faf11f72fe8fee
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-17 11:38:24 +0100

    Added Google Tag Manager

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -19,8 +19,31 @@ export default function LocaleLayout({ children, params: { locale } }) {
         data-cmp-cdn="cdn.consentmanager.net"
         data-cmp-codesrc="1"
       />
-
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WM9T5LT8');
+          `,
+        }}
+      />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WM9T5LT8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Nav locale={locale} />
         {children}
       </body>
