ebd43284e31e4020558bb0efb220fb62ea15efdb
Mark Julien Hahn <mail@markjulienhahn.de>
2024-05-15 17:35:55 +0200

    ...

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,9 +1,20 @@
 import Homepage from "../../../components/_Home/Homepage";
+import { getLivestream, getHome, getProgram } from "../../../sanity/sanity-utils";
 
 export default async function Home({ params: { locale } }) {
+  const livestream = await getLivestream();
+  const home = await getHome();
+  const program = await getProgram();
   return (
     <main>
-      <Homepage locale={locale} />
+      <Homepage
+        locale={locale}
+        livestream={livestream}
+        home={home}
+        program={program}
+      />
     </main>
   );
 }
+
+export const revalidate = 10;
